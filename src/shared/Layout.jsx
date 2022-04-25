3a3669e338140234be8ecf82150e91a8a3fcc2e0
J0shcodes <mosesjoshua350@gmail.com>
2022-04-24 20:09:43 -0700

    removed unused variables

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import MainHeader from "./MainHeader";
-import AuthHeader from "./AuthHeader";
 import classes from './Layout.module.css'
 
 
 const Layout = props => {
-    // const [loggedIn, setLoggedIn] = useState(false);
-
     return (      
         <div className={classes.layout}>
-            {/* {loggedIn ? <MainHeader/> : <AuthHeader/>} */}
             <main>{props.children}</main>
         </div>   
     )
