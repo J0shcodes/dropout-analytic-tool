3bebfa3b4baaf093dc905d0e17aad11de0aefc65
J0shcodes <mosesjoshua350@gmail.com>
2022-04-24 20:56:03 -0700

    removed the login button

--- src/shared/AuthHeader.jsx
+++ src/shared/AuthHeader.jsx
@@ -4,7 +4,7 @@ import classes from './AuthHeader.module.css'
 const AuthHeader = () => {
     return (
         <header className={classes.navbar}>
-            <Link to="/login" className={classes.login}>Log in</Link>
+            {/* <Link to="/login" className={classes.login}>Log in</Link> */}
             <Link to="/student_signup" className={classes.signup}>Student Sign up</Link>
             <Link to="/teacher_signup" className={classes.signup}>Teacher Sign up</Link>
         </header>
