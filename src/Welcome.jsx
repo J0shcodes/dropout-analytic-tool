02f776e1568b7a9c9f6bafc4e423a1eeaa7f1b7f
J0shcodes <mosesjoshua350@gmail.com>
2022-04-25 03:12:38 -0700

    welcome page now redirects to the required signup page once a user selects an input and submits form

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,10 +1,28 @@
+import {useState} from 'react';
+
 import { Link } from "react-router-dom";
 import Layout from "./shared/Layout";
 import AuthHeader from "./shared/AuthHeader";
 import classes from "./styles/Welcome.module.css";
 import Button from "./shared/Button";
 
+import {useNavigate} from "react-router-dom"
+
 const Welcome = () => {
+  const [selectedOption, setSelectedOption] = useState('')
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    setSelectedOption(e.target.value)
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    
+    if(selectedOption === 'Student') navigate('/student_signup');
+    if(selectedOption === 'Teacher') navigate('/teacher_signup');
+  }
+
   return (
     <>
       <AuthHeader />
@@ -15,7 +33,7 @@ const Welcome = () => {
             Already have an account? <Link to="/login" className={classes.link}>Login</Link>
           </p>
           <div className={classes.form}>
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <p className={classes.category}>What category do you classify as?</p>
               <div>
                 <div style={{marginTop: "0.4em"}}>
@@ -23,21 +41,23 @@ const Welcome = () => {
                     type="radio"
                     id="category1"
                     name="category"
-                    value="student"
+                    value="Student"
+                    onChange={handleChange}
                   />
-                  <label for="category1">Student</label>
+                  <label htmlFor="category1">Student</label>
                 </div>
                 <div style={{marginTop: "0.4em"}}>
                   <input
                     type="radio"
                     id="category2"
                     name="category"
-                    value="teacher"
+                    value="Teacher"
+                    onChange={handleChange}
                   />
-                  <label for="category2">Teacher</label>
+                  <label htmlFor="category2">Teacher</label>
                 </div>
               </div>
-              <Button children="Sign up"/>
+              <Button type="submit" children="Sign up"/>
             </form>
           </div>
         </div>
